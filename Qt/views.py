a15f6cc03534701f2b1fe315dd25e691b2cf4622
sriharsha1608 <40089518+sriharsha1608@users.noreply.github.com>
2020-12-10 22:30:59 +0530

    changed spelling push

--- Qt/views.py
+++ Qt/views.py
@@ -5,6 +5,7 @@ from .models import Post,Item,Branch,Transactions,Inventory
 from django.contrib.auth.models import User,auth
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
 
 #@csrf_exempt
 #@login_required
@@ -38,11 +39,11 @@ def inventory(request):
     else:
         return  render(request,'login.html')'''
     if request.method == 'POST':
-        if request.POST.get('item_name') and request.POST.get('item_quantity') and request.POST.get('branch'):
+        if request.POST.get('item') and request.POST.get('quantity') and request.POST.get('branch'):
             inventory=Inventory()
-            inventory.Item_Name= request.POST.get('item_name')
-            inventory.Item_Quantity= request.POST.get('item_quantity')
-            inventory.branch=request.POST.get('branch_code')
+            inventory.item= request.POST.get('item')
+            inventory.quantity= request.POST.get('quantity')
+            inventory.branch=request.POST.get('branch')
             inventory.save()
 
             return redirect('/')  
@@ -59,7 +60,7 @@ def branch(request):
     if request.method == 'POST':
         if request.POST.get('Branch_code') and request.POST.get('Branch_Name') and request.POST.get('Branch_Addres'):
             branch=Branch()
-            branch.Branch_code = request.POST.get('Branch_code')
+            branch.Branch_Code = request.POST.get('Branch_Code')
             branch.Branch_Name = request.POST.get('Branch_Name')
             branch.Branch_Addres = request.POST.get('Branch_Addres')
             branch.save()
@@ -102,8 +103,8 @@ def transaction(request):
             transaction.Quantity = t_quantity
             transaction.save()
 
-            source_inventory = Inventory.objects.get(branch_Branch_Code=t_source_branch, item_id=t_item_id)
-            destination_inventory = Inventory.objects.get(branch_Branch_Code=t_destination_branch, item_id=t_item_id)
+            source_inventory = Inventory.objects.get(branch__Branch_Code=t_source_branch, item_id=t_item_id)
+            destination_inventory = Inventory.objects.get(branch__Branch_Code=t_destination_branch, item_id=t_item_id)
             if request.POST.get('Transaction_Type') == 'DEBIT':
               source_inventory.quantity -= t_quantity
               destination_inventory.quantity += t_quantity
